89ee95f54a45265ec603bc21352d0905f0c54885
jake-freeman <jfreeman@hawk.iit.edu>
2016-08-02 13:10:29 -0500

    Fix a few pins

--- Arduino/IIT_HASP_Payload/HASP_Code.h
+++ Arduino/IIT_HASP_Payload/HASP_Code.h
@@ -21,8 +21,27 @@
 #define SENSARRAY_CURRENT     4
 
 
-#define CURRENTSENSOR_PIN     2  // analog
-#define CAMERA_PIN           51
-#define GEIGER_PIN           22
+#define CURRENTSENSOR_PIN     0  // analog
+#define CAMERA_PIN           35
+#define GYRO_PIN             30
+#define GEIGER_PIN           40
+
+/**
+ * GPS Enable - D1 (unused)
+ * Card Reader Tx1 - D18
+ * Card Reader Rx1 - D19
+ * SDN - A6
+ * NIRQ - A5
+ * Card Select 4 - A4
+ * Card Select 3 - A3
+ * CS2 (tranceiver) - A2 (would need to solder CLK, uses SPI)
+ * CS1 - A1
+ * Current Sensor - A0
+ * MPU6050 - SCL1, SDA1
+ * Geiger - A1 (soldered to D30)
+ * MS58603 - SDA1, SCL1
+ * Copernicus - Tx1 (D18), Rx1(D19)
+ * HackHD - solder pin D35
+ */
 
 #endif
